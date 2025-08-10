be4f38892bb9ab45008608f650699c75d82e1ea8
YashMasane <masaneyash6@gmail.com>
2025-08-10 22:55:22 +0530

    added sqlite database

--- backend.py
+++ backend.py
@@ -1,23 +1,29 @@
 from langgraph.graph import StateGraph, START, END
 from langchain_core.messages import BaseMessage
 from langgraph.graph import add_messages
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from typing import TypedDict, Annotated
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+import sqlite3
 
 load_dotenv()
 
 model = ChatOpenAI()
 
+
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
+
 
 def chat_node(state: ChatState):
     response = model.invoke(state['messages'])
     return {'messages': [response]}
 
-checkpointer = InMemorySaver()
+
+conn = sqlite3.connect('chatbot.db', check_same_thread=False)
+
+checkpointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 
@@ -26,3 +32,12 @@ graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
 
 workflow = graph.compile(checkpointer=checkpointer)
+
+
+def get_thread_id():
+    thread_ids = set()
+    for checkpoint in checkpointer.list(None):
+        thread_ids.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(thread_ids)
+
--- frontend.py
+++ frontend.py
@@ -1,130 +1,100 @@
 import streamlit as st
+from backend import workflow, get_thread_id
 from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from backend import workflow
 import uuid
 
-# ***************************** utility functions *************************************************
+# **************************************** utility functions *************************
 
 
-def generate_threadid():
+def generate_thread_id():
     thread_id = uuid.uuid4()
     return thread_id
 
 
-def add_chat_thread(thread_id):
-    if thread_id not in st.session_state['chat_thread']:
-        st.session_state['chat_thread'].append(thread_id)
-
-
 def reset_chat():
-    thread_id = generate_threadid()
+    thread_id = generate_thread_id()
     st.session_state['thread_id'] = thread_id
-    add_chat_thread(st.session_state['thread_id'])
-    st.session_state['chat_history'] = []
+    add_thread(st.session_state['thread_id'])
+    st.session_state['message_history'] = []
 
 
-def load_messages(thread_id):
+def add_thread(thread_id):
+    if thread_id not in st.session_state['chat_threads']:
+        st.session_state['chat_threads'].append(thread_id)
+
+
+def load_conversation(thread_id):
     return workflow.get_state(config={'configurable': {'thread_id': thread_id}}).values['messages']
 
 
-def generate_chat_name(input_str, name_list):
-    llm = ChatOpenAI()
-    template = (
-        "You are a helpful assistant. Generate a name based on the following user input:\n"
-        "{user_input}\n\n"
-        "The name must be a maximum of five words.\n"
-        "Make sure the name is different from the following list:\n{existing_names}"
-    )
-
-    prompt_template = PromptTemplate.from_template(template)
-    if isinstance(name_list, list):
-        name_list_str = ", ".join(name_list)
-    else:
-        name_list_str = str(name_list)
-    prompt = prompt_template.format(
-        user_input=input_str, existing_names=name_list_str)
-    name = llm.invoke(prompt)
-    return name.content
-
-
-# ********************************* Session State ************************************
-
-
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history'] = []
+# **************************************** Session Setup ******************************
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
 
 if 'thread_id' not in st.session_state:
-    st.session_state['thread_id'] = generate_threadid()
+    st.session_state['thread_id'] = generate_thread_id()
 
-if 'chat_thread' not in st.session_state:
-    st.session_state['chat_thread'] = []
+if 'chat_threads' not in st.session_state:
+    st.session_state['chat_threads'] = get_thread_id()
 
-add_chat_thread(st.session_state['thread_id'])
-
-if 'chat_names' not in st.session_state:
-    st.session_state['chat_names'] = {}
+add_thread(st.session_state['thread_id'])
 
 
-# ********************************** main UI ***********************************
+# **************************************** Sidebar UI *********************************
 
-st.sidebar.title('LangGraph ChatBot')
+st.sidebar.title('LangGraph Chatbot')
 
 if st.sidebar.button('New Chat'):
     reset_chat()
 
-st.sidebar.header('Your Chats')
+st.sidebar.header('My Conversations')
 
-for thread_id in st.session_state['chat_thread'][::-1]:
-
-    chat_name = st.session_state['chat_names'].get(thread_id, str(thread_id)) 
-
-    if st.sidebar.button(chat_name, key=str(thread_id)):
+for thread_id in st.session_state['chat_threads'][::-1]:
+    if st.sidebar.button(str(thread_id)):
         st.session_state['thread_id'] = thread_id
-        messages = load_messages(st.session_state['thread_id'])
+        messages = load_conversation(thread_id)
 
         temp_messages = []
+
         for msg in messages:
             if isinstance(msg, HumanMessage):
                 role = 'user'
             else:
                 role = 'assistant'
-
             temp_messages.append({'role': role, 'content': msg.content})
-        st.session_state['chat_history'] = temp_messages
 
-CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+        st.session_state['message_history'] = temp_messages
 
-for messages_history in st.session_state['chat_history']:
-    with st.chat_message(messages_history['role']):
-        st.text(messages_history['content'])
+
+# **************************************** Main UI ************************************
+
+# loading the conversation history
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
 
 user_input = st.chat_input('Type here')
 
 if user_input:
 
-    if len(st.session_state['chat_history']) == 0:
-        chat_name = generate_chat_name(user_input, list(
-            st.session_state['chat_names'].values()))
-        st.session_state['chat_names'][st.session_state['thread_id']] = chat_name
-
-    st.session_state['chat_history'].append(
+    # first add the message to message_history
+    st.session_state['message_history'].append(
         {'role': 'user', 'content': user_input})
     with st.chat_message('user'):
         st.text(user_input)
 
-    # response = workflow.invoke({'messages': HumanMessage(user_input)}, config=CONFIG)
-    # ai_response = response['messages'][-1].content
-    # st.session_state['chat_history'].append({'role': 'assistant', 'content': ai_response})
+    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
+
+    # first add the message to message_history
     with st.chat_message('assistant'):
-        ai_mesasge = st.write_stream(
-            message.content for message, metadata in workflow.stream(
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in workflow.stream(
                 {'messages': [HumanMessage(content=user_input)]},
                 config=CONFIG,
                 stream_mode='messages'
             )
         )
 
-    st.session_state['chat_history'].append(
-        {'role': 'assistant', 'content': ai_mesasge})
+    st.session_state['message_history'].append(
+        {'role': 'assistant', 'content': ai_message})
